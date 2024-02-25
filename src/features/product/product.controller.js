dbbc92580247c0480c45971fac36da7a4325aae9
paulsp01 <paulswarnalee@gmail.com>
2024-02-24 20:17:05 -0500

    api for get  filter product

--- src/features/product/product.controller.js
+++ src/features/product/product.controller.js
@@ -1,35 +1,43 @@
 import ProductModel from "./product.model.js";
 
-export default class ProductController{
+export default class ProductController {
+  getAllProducts(req, res) {
+    const product = ProductModel.getAll();
+    res.status(200).send(product);
+  }
 
-    getAllProducts(req,res){
-        const product=ProductModel.GetAll();
-        res.status(200).send(product);
+  addProduct(req, res) {
+    const { name, price, sizes } = req.body;
+    const newProduct = {
+      name,
+      price: parseFloat(price),
+      sizes: sizes.split(","),
+      imageUrl: req.file.filename,
+    };
+    const createdRecord = ProductModel.add(newProduct);
+    res.status(201).send(createdRecord);
+  }
+
+  rateProduct(req, res) {}
+
+  getoneproduct(req, res) {
+    const id = req.params.id;
+   
+    const product = ProductModel.get(id);
+
+    if (!product) {
+      res.status(404).send("Product not found");
+    } else {
+      return res.status(200).send(product);
     }
+  }
+  filterProducts(req, res) {
+    console.log("print" + req.query);
+    const minPrice = req.query.minPrice;
+    const maxPrice = req.query.maxPrice;
+    const category = req.query.category;
+    const result = ProductModel.filter(minPrice, maxPrice, category);
 
-    addProduct(req, res){
-       const {name,price,sizes}=req.body;
-       const newProduct={
-        name,
-        price:parseFloat(price),
-        sizes:sizes.split(','),
-        imageUrl:req.file.filename
-       }
-         const createdRecord = ProductModel.add(newProduct);
-         res.status(201).send(createdRecord);
-
-    }
-
-    rateProduct(req,res){}
-
-   getoneproduct(req,res){
-    const id=req.params.id;
-    const product=ProductModel.get(id);
-    if(!product){
-        res.status(404).send('product not found');
-    }else{
-        return res.status(200).send(product);
-    }
-   }
-
+    res.status(200).send(result);
+  }
 }
--- src/features/product/product.routes.js
+++ src/features/product/product.routes.js
@@ -10,8 +10,13 @@ const productRouter=express.Router();
 const productController=new ProductController();
 
 //all the origin paths to the controller methods
+productRouter.get("/filter", productController.filterProducts);
+
+
 productRouter.get("/", productController.getAllProducts);
 productRouter.post("/", upload.single('imageUrl') ,productController.addProduct);
 productRouter.get("/:id",productController.getoneproduct);
+
+
 
 export default productRouter;
